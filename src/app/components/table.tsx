24b1f7f0712ec1d8371279f3fd6552d11d4340ae
byessilyurt <y.yesilyurt14@gmail.com>
2025-08-24 18:31:55 +0000

    24 Aug 2025, 20:31

--- src/app/components/table.tsx
+++ src/app/components/table.tsx
@@ -8,7 +8,20 @@ export default function Table() {
                         <th>Age</th>
                         <th>Gender</th>
                     </tr>
+
                 </thead>
+                <tbody>
+                    <tr>
+                        <td>John</td>
+                        <td>25</td>
+                        <td>Male</td>
+                    </tr>
+                    <tr>
+                        <td>Jane</td>
+                        <td>26</td>
+                        <td>Female</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
