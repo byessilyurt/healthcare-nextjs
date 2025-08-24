e5bb7e33c8cbf70508391acf7ee4b1512861b769
byessilyurt <y.yesilyurt14@gmail.com>
2025-08-24 18:28:50 +0000

    24 Aug 2025, 20:28

---
+++ src/app/components/table.tsx
@@ -0,0 +1,15 @@
+export default function Table() {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Gender</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    )
+}
